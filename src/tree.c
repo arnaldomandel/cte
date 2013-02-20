b05a8f4a014e2a087dbe505679cb64d100c83844
Andre Onuki dos Santos <onuki@ime.usp.br>
2013-02-20 10:17:26 -0300

    Simplification of tree handling

--- src/tree.c
+++ src/tree.c
@@ -15,26 +15,20 @@ Tree_node* create_node_child(Tree_node* parent, char symbol, int type);
  * Searches for a node with the given symbol among the child of the given node.
  */
 Tree_node* get_create_node_child(Tree_node* parent, char symbol, int type) {
-  // parent node has no child, so we create its first child
-  if (parent->child == NULL) {
-    parent->child = create_node_child(parent, symbol, type);
-    return parent->child;
-  }
-
   // walk through every child node, until we find a corresponding symbol, or stop at last node
   Tree_node* current_node = parent->child;
-  while(current_node->symbol != symbol && current_node->sibling != NULL) {
+  while(current_node != NULL) {
+    if (current_node->symbol == symbol) { // found correct node
+      return current_node;
+    }
     current_node = current_node->sibling;
   }
 
-  // the correct node was found on the while loop
-  if (current_node->symbol == symbol) {
-    return current_node;
-  }
-
   // the correct node was not found, and current node is the last node
-  current_node->sibling = create_node_child(parent, symbol, type);
-  return current_node->sibling;
+  Tree_node* new_node = create_node_child(parent, symbol, type);
+  new_node->sibling = parent->child;
+  parent->child = new_node;
+  return new_node;
 }
 
 /*
